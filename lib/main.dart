25318dc887449b0d4ff97ecdc40fb35754d8c0d8
kikuuchi <kikuuchi@itcowork.co.jp>
2024-02-18 20:00:42 +0900

    feat: サイドメニューを表示するための処理の実装

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,40 @@ class MyApp extends StatelessWidget {
             '機器管理システム',
             style: TextStyle(color: Colors.white),
           ),
+          iconTheme: IconThemeData(color: Colors.white),
           backgroundColor: Colors.blue,
+        ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                child: Text(
+                  '管理一覧',
+                  style: TextStyle(color: Colors.white),
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+              ),
+              ListTile(
+                title: Text("PC一覧"),
+                leading: Icon(Icons.computer),
+              ),
+              ListTile(
+                title: Text("iOS一覧"),
+                leading: Icon(Icons.apple),
+              ),
+              ListTile(
+                title: Text("Android一覧"),
+                leading: Icon(Icons.android),
+              ),
+              ListTile(
+                title: Text("Windows一覧"),
+                leading: Icon(Icons.tablet_android),
+              ),
+            ],
+          ),
         ),
       ),
     );
