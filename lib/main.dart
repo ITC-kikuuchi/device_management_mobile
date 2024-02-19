983049306bb794c8056bceaeb4f3fa0f6a16ba6b
kikuuchi <kikuuchi@itcowork.co.jp>
2024-02-19 17:14:19 +0900

    fix: 不要な処理の削除

--- lib/main.dart
+++ lib/main.dart
@@ -10,48 +10,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            '機器管理システム',
-            style: TextStyle(color: Colors.white),
-          ),
-          iconTheme: IconThemeData(color: Colors.white),
-          backgroundColor: Colors.blue,
-        ),
-        drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: <Widget>[
-              DrawerHeader(
-                child: Text(
-                  '管理一覧',
-                  style: TextStyle(color: Colors.white),
-                ),
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                ),
-              ),
-              ListTile(
-                title: Text("PC一覧"),
-                leading: Icon(Icons.computer),
-              ),
-              ListTile(
-                title: Text("iOS一覧"),
-                leading: Icon(Icons.apple),
-              ),
-              ListTile(
-                title: Text("Android一覧"),
-                leading: Icon(Icons.android),
-              ),
-              ListTile(
-                title: Text("Windows一覧"),
-                leading: Icon(Icons.tablet_android),
-              ),
-            ],
-          ),
-        ),
-      ),
     );
   }
 }
