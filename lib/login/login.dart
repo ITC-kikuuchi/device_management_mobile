146b0973452a7b98967227bec28d063c911df1ae
kikuuchi <kikuuchi@itcowork.co.jp>
2024-02-23 20:15:20 +0900

    feat: ログイン画面に必要な機能の実装

--- lib/login/login.dart
+++ lib/login/login.dart
@@ -11,5 +11,51 @@ class _LoginPage extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold{}
+    return Scaffold(
+      appBar: MyAppBar(),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(40.0), // ウィジェットの周囲に余白を追加
+          child: Column( // 子要素を垂直方向に配置
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                child: TextFormField( // テキスト入力フィールドの作成
+                  decoration: const InputDecoration(
+                    labelText: 'メールアドレスを入力してください',
+                  ),
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                child: TextFormField( // テキスト入力フィールドの作成
+                  obscureText: _isObscure, // テキストを非表示にするかどうかの設定
+                  decoration: InputDecoration(
+                      labelText: 'パスワード',
+                      suffixIcon: IconButton(
+                          icon: Icon(_isObscure
+                              ? Icons.visibility_off
+                              : Icons.visibility),
+                          onPressed: () { // アイコンがタップされた時の処理
+                            setState(() { // ステートを更新する
+                              _isObscure = !_isObscure;
+                            });
+                          })),
+                ),
+              ),
+              Center( // ボタンを画面中央に配置
+                child: ElevatedButton(
+                  onPressed: () {}, // ボタンが押下された時の処理
+                  child: Text('ログイン'),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
