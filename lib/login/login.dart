c30acba1896c9c7006d56a981c0442fa921270ba
kikuuchi <kikuuchi@itcowork.co.jp>
2024-02-23 20:06:20 +0900

    feat: import 及び class の追加

---
+++ lib/login/login.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import '../app_bar/app_bar.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPage createState() => _LoginPage();
+}
+
+class _LoginPage extends State<LoginPage> {
+  bool _isObscure = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold{}
+}
