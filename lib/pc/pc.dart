f2509c8b20c1270cd976671e07b3fb18ed3dee71
kikuuchi <kikuuchi@itcowork.co.jp>
2024-02-24 15:57:59 +0900

    feat: import 処理とクラスの実装

---
+++ lib/pc/pc.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import '../app_bar/app_bar.dart';
+import '../drawer/drawer.dart';
+
+class pcPage extends StatefulWidget {
+  @override
+  _pcPage createState() => _pcPage();
+}
+
+class _pcPage extends State<pcPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    );
+  }
+}
