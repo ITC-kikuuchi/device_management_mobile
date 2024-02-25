9da29ab23fa8f373c9e6a7cf61d7be0d63a46910
kikuuchi <kikuuchi@itcowork.co.jp>
2024-02-25 18:49:09 +0900

    feat: ログアウトするためのアイコン及び、ログイン画面に遷移するための処理の実装

--- lib/app_bar/app_bar.dart
+++ lib/app_bar/app_bar.dart
@@ -1,6 +1,37 @@
 import 'package:flutter/material.dart';
+import '../login/login.dart';
 
 class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: const Text(
+        '機器管理システム',
+        style: TextStyle(color: Colors.white),
+      ),
+      actions: [
+        IconButton(
+          icon: Icon(
+            Icons.logout, // ログアウトアイコンを指定します
+            color: Colors.white,
+          ),
+          onPressed: () {
+            Navigator.pushAndRemoveUntil(
+              context,
+              MaterialPageRoute(builder: (context) => LoginPage()),
+              (route) => false,
+            );
+          },
+        ),
+      ],
+      iconTheme: IconThemeData(color: Colors.white),
+      backgroundColor: Colors.blue,
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+}
   @override
   Widget build(BuildContext context) {
     return AppBar(
