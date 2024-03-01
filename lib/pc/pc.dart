8fb89455406c47d08e71271183d1024b8070eabc
kikuuchi <kikuuchi@itcowork.co.jp>
2024-03-01 15:38:18 +0900

    feat: shared_preferences インストール

--- lib/pc/pc.dart
+++ lib/pc/pc.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../app_bar/app_bar.dart';
 import '../drawer/drawer.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class pcPage extends StatefulWidget {
   @override
@@ -8,12 +9,31 @@ class pcPage extends StatefulWidget {
 }
 
 class _pcPage extends State<pcPage> {
+  Future<String> getAccessToken() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString('access_token') ?? "";
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: MyAppBar(),
       drawer: MyDrawer(),
+      body: Center(
+        child: FutureBuilder<String>(
+          future: getAccessToken(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return CircularProgressIndicator();
+            }
+            if (snapshot.hasData) {
+              // トークンを画面に表示
+              return Text('Access Token: ${snapshot.data}');
+            }
+            return Text('Access Token not found');
+          },
+        ),
+      ),
     );
   }
 }
