63e131cc8cb091f479b9f5dfd9f23e69b969ad56
kikuuchi <kikuuchi@itcowork.co.jp>
2024-03-13 15:01:20 +0900

    feat: クラスの実装

--- lib/ios/ios_detail.dart
+++ lib/ios/ios_detail.dart
@@ -5,3 +5,10 @@ import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../app_bar/app_bar.dart';
+
+class iosDetailPage extends StatefulWidget {
+  final int iosId;
+  iosDetailPage({required this.iosId});
+  @override
+  _iosDetailPage createState() => _iosDetailPage();
+}
