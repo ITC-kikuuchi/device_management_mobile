b89aabeab8b8a59fbeebe43d7e20df31768c87c3
kikuuchi <kikuuchi@itcowork.co.jp>
2024-03-14 13:43:55 +0900

    feat: Android 一覧取得処理の実装

--- lib/android/android.dart
+++ lib/android/android.dart
@@ -37,6 +37,47 @@ class _androidPage extends State<androidPage> {
     final prefs = await SharedPreferences.getInstance();
     accessToken = prefs.getString('access_token') ?? "";
   }
+
+  /**
+   * Android一覧取得
+   */
+  Future<String> _getAndroid() async {
+    try {
+      final response = await http.get(
+        Uri.parse('http://localhost:3001/android'),
+        headers: <String, String>{
+          'Authorization': 'Bearer $accessToken',
+        },
+      );
+      if (response.statusCode == 200) {
+        final String responseBody = utf8.decode(response.bodyBytes);
+        final List<dynamic> data = json.decode(responseBody);
+        setState(() {
+          androidList = data
+              .map((android) => {
+                    'id': android['id'],
+                    'label_name': android['label_name'] != null
+                        ? android['label_name'] as String
+                        : '',
+                    'os': android['os'] != null ? android['os'] as String : '',
+                    'delete_flag': android['delete_flag'] != null
+                        ? android['delete_flag'] as bool
+                        : false,
+                    'last_updated_flag': android['last_updated_flag'] != null
+                        ? android['last_updated_flag'] as bool
+                        : false,
+                  })
+              .toList();
+        });
+      } else {
+        throw Exception('Failed to load data');
+      }
+      return '';
+    } catch (e) {
+      print('Error fetching Android data: $e');
+      return '';
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
