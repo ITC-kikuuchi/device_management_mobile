6bfc4c1402abed029344abee99352c26f8cc84db
kikuuchi <kikuuchi@itcowork.co.jp>
2024-03-14 13:43:18 +0900

    feat: ログインユーザのアクセストークンを取得する処理の実装

--- lib/android/android.dart
+++ lib/android/android.dart
@@ -29,6 +29,14 @@ class _androidPage extends State<androidPage> {
     await _getAccessToken();
     await _getAndroid();
   }
+
+  /**
+   * ログインユーザのトークン取得
+   */
+  Future<void> _getAccessToken() async {
+    final prefs = await SharedPreferences.getInstance();
+    accessToken = prefs.getString('access_token') ?? "";
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
