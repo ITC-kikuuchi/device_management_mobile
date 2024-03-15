4191f952148251945bee30b4196358d4ff626a6e
kikuuchi <kikuuchi@itcowork.co.jp>
2024-03-15 10:47:29 +0900

    feat: import文の追加

--- lib/android/android.dart
+++ lib/android/android.dart
@@ -6,6 +6,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 import '../app_bar/app_bar.dart';
 import '../drawer/drawer.dart';
+import '../android/android_detail.dart';
 
 class androidPage extends StatefulWidget {
   @override
