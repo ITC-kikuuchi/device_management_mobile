0dab1e23f3f559c79c572b4d2dc3bc66ba968465
kikuuchi <kikuuchi@itcowork.co.jp>
2024-03-16 19:27:21 +0900

    feat: ios 最終更新者取得処理の実装

--- lib/ios/ios.dart
+++ lib/ios/ios.dart
@@ -83,6 +83,32 @@ class _iosPage extends State<iosPage> {
   }
 
   /**
+   * ios最終更新者取得
+   */
+  Future<String> _getLastUpdatedUser() async {
+    try {
+      final response = await http.get(
+        Uri.parse('http://localhost:3001/ios_update_user'),
+        headers: <String, String>{
+          'Authorization': 'Bearer $accessToken',
+        },
+      );
+      if (response.statusCode == 200) {
+        final String responseBody = utf8.decode(response.bodyBytes);
+        setState(() {
+          userData = json.decode(responseBody);
+        });
+      } else {
+        throw Exception('Failed to load data');
+      }
+      return '';
+    } catch (e) {
+      print('Error fetching user data: $e');
+      return '';
+    }
+  }
+
+  /**
    * ios一覧を表示する処理
    */
   Widget _buildIosCards() {
