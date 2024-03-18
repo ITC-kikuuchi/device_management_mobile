bb4e2362b922676a4290c4111e0e06e2ab9ba4e6
kikuuchi <kikuuchi@itcowork.co.jp>
2024-03-18 15:28:52 +0900

    feat: import 文の追加

--- lib/widgets/card_list.dart
+++ lib/widgets/card_list.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import '../pc/pc_detail.dart';
 import '../ios/ios_detail.dart';
 import '../android/android_detail.dart';
+import '../constants.dart';
 
 class CardList extends StatefulWidget {
   late List<Map<String, dynamic>> deviceList;
