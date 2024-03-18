4cf53ae5881767a36e47dc4d1c9657d1f78c8913
kikuuchi <kikuuchi@itcowork.co.jp>
2024-03-18 14:12:51 +0900

    fix: 不要な処理の削除

--- lib/android/android.dart
+++ lib/android/android.dart
@@ -156,35 +156,6 @@ class _androidPage extends State<androidPage> {
     );
   }
 
-  /**
-   * android最終更新者を表示する処理
-   */
-  Widget _buildLastUpdatedUser() {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(15, 8, 0, 8),
-      child: RichText(
-        text: TextSpan(
-          children: [
-            TextSpan(
-              text: '最終更新者:',
-              style: TextStyle(
-                fontSize: 18,
-                color: Colors.black,
-              ),
-            ),
-            TextSpan(
-              text: '${userData['user_name'] ?? ''}',
-              style: TextStyle(
-                fontSize: 18,
-                color: Colors.red,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/ios/ios.dart
+++ lib/ios/ios.dart
@@ -154,35 +154,6 @@ class _iosPage extends State<iosPage> {
     );
   }
 
-  /**
-   * ios最終更新者を表示する処理
-   */
-  Widget _buildLastUpdatedUser() {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(15, 8, 0, 8),
-      child: RichText(
-        text: TextSpan(
-          children: [
-            TextSpan(
-              text: '最終更新者:',
-              style: TextStyle(
-                fontSize: 18,
-                color: Colors.black,
-              ),
-            ),
-            TextSpan(
-              text: '${userData['user_name'] ?? ''}',
-              style: TextStyle(
-                fontSize: 18,
-                color: Colors.red,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
