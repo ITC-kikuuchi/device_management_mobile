e0f1bcfe048124627d7ba5098da9e609860c29cb
kikuuchi <kikuuchi@itcowork.co.jp>
2024-03-18 16:25:28 +0900

    fix: 不要な処理の削除

--- lib/android/android_detail.dart
+++ lib/android/android_detail.dart
@@ -13,45 +13,6 @@ class androidDetailPage extends StatefulWidget {
   _androidDetailPage createState() => _androidDetailPage();
 }
 
-// Android情報を表示するウィジェット
-class AndroidInfoItem extends StatelessWidget {
-  final String label;
-  final String value;
-
-  const AndroidInfoItem({
-    required this.label,
-    required this.value,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(25, 8, 25, 5), // 左側に16の余白を追加
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // ラベルを左寄せに配置
-          Text(
-            label,
-            style: TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          SizedBox(height: 4), // ラベルと値の間隔を設定
-          // 値を左寄せに配置
-          Text(
-            value,
-            style: TextStyle(
-              fontSize: 18,
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
 class _androidDetailPage extends State<androidDetailPage> {
   late String accessToken;
   Map<String, dynamic> androidData = {};
--- lib/ios/ios_detail.dart
+++ lib/ios/ios_detail.dart
@@ -13,45 +13,6 @@ class iosDetailPage extends StatefulWidget {
   _iosDetailPage createState() => _iosDetailPage();
 }
 
-// ios情報を表示するウィジェット
-class IosInfoItem extends StatelessWidget {
-  final String label;
-  final String value;
-
-  const IosInfoItem({
-    required this.label,
-    required this.value,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(25, 8, 25, 5), // 左側に16の余白を追加
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // ラベルを左寄せに配置
-          Text(
-            label,
-            style: TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          SizedBox(height: 4), // ラベルと値の間隔を設定
-          // 値を左寄せに配置
-          Text(
-            value,
-            style: TextStyle(
-              fontSize: 18,
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
 class _iosDetailPage extends State<iosDetailPage> {
   late String accessToken;
   Map<String, dynamic> iosData = {};
