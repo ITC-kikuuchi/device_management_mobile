e8cfbb97065f0fc5928bb8b83a064406e0a02f74
kikuuchi <kikuuchi@itcowork.co.jp>
2024-03-19 17:14:07 +0900

    fix: 項目と項目の間に線を表示するための処理の実装

--- lib/widgets/detail_item.dart
+++ lib/widgets/detail_item.dart
@@ -14,17 +14,29 @@ class DetailItem extends StatefulWidget {
 class _DetailItem extends State<DetailItem> {
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
     return Padding(
       padding: const EdgeInsets.fromLTRB(25, 8, 25, 5), // 左側に16の余白を追加
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           // ラベルを左寄せに配置
-          Text(
-            widget.label,
-            style: TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
+          Container(
+            width: screenWidth,
+            decoration: BoxDecoration(
+              border: Border(
+                top: BorderSide(width: 1.0, color: const Color.fromARGB(255, 192, 192, 192)),
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.only(top: 4), // 上側に余白を追加
+              child: Text(
+                widget.label,
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
             ),
           ),
           SizedBox(height: 4), // ラベルと値の間隔を設定
