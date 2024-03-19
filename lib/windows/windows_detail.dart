3e61015a4abd4996ff4005b9289fa8ea30c9ef38
kikuuchi <kikuuchi@itcowork.co.jp>
2024-03-19 14:05:20 +0900

    feat: 画面を仮表示させるための処理の実装

--- lib/windows/windows_detail.dart
+++ lib/windows/windows_detail.dart
@@ -1,3 +1,20 @@
 import 'package:flutter/material.dart';
 
 import '../app_bar/app_bar.dart';
+
+class windowsDetailPage extends StatefulWidget {
+  final int windowsId;
+  windowsDetailPage({required this.windowsId});
+  @override
+  _windowsDetailPage createState() => _windowsDetailPage();
+}
+
+class _windowsDetailPage extends State<windowsDetailPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: MyAppBar(),
+    );
+  }
+}
