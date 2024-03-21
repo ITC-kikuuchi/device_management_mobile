f7f01e08952b0b06d6929183019da683dcab0470
kikuuchi <kikuuchi@itcowork.co.jp>
2024-03-21 10:34:17 +0900

    feat: レスポンスのステータスコードの定数の実装

--- lib/constants.dart
+++ lib/constants.dart
@@ -7,3 +7,14 @@ class DeviceId {
   static const android = 3;
   static const windows = 4;
 }
+
+/**
+ * レスポンスのステータスコードを管理する定数
+ */
+class HttpStatusCode {
+  static const ok = 200;
+  static const unauthorized = 401;
+  static const forbidden = 403;
+  static const not_found = 404;
+  static const internal_server_error = 500;
+}
