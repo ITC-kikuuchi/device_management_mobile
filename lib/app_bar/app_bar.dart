6b6aff7bba1cd666337ad406767856a9d4863014
kikuuchi <kikuuchi@itcowork.co.jp>
2024-03-23 20:45:24 +0900

    feat: ダイアログの枠線を表示する処理の追加

--- lib/app_bar/app_bar.dart
+++ lib/app_bar/app_bar.dart
@@ -60,6 +60,11 @@ void _showDialog(BuildContext context) {
         onTap: () => Navigator.pop(context),
         child: Dialog(
           backgroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            // 枠線を追加する
+            borderRadius: BorderRadius.circular(10.0), // 枠線の角丸を設定
+            side: BorderSide(color: const Color.fromARGB(255, 106, 106, 106), width: 3.0), // 枠線の色と幅を設定
+          ),
           child: Container(
             width: double.infinity,
             padding: const EdgeInsets.all(20.0),
